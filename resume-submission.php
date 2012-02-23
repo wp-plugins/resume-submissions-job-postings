2b57386076492b50a467f944bef4848040e27993
kandrews <kandrews@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-23 16:12:42 +0000

    Updated info
    git-svn-id: https://plugins.svn.wordpress.org/resume-submissions-job-postings/trunk@509425 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- resume-submission.php
+++ resume-submission.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Resumé Submissions & Job Postings
 Plugin URI: http://www.geerservices.com/wordpress-plugins/resume-jobs/
-Description: Allows the admin to create and show job postings. Users can submit their resume to a job posting, or submit it just because. 
+Description: Allows the admin to create and show job postings. Users can submit their resume in response to a posting or for general purposes. 
 Version: 1.8.8.1
 Author: Keith Andrews (GSI)
 Author URI: http://www.geerservices.com
